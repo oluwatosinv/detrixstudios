74130e6322728408dc7fc1cde7fcb83848c18274
faboade oluwatosin <30568915+oluwatosinv@users.noreply.github.com>
2021-10-31 13:59:05 +0100

    fix mdboostrap
    breaking css fixed

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,7 @@
 import Layout from '../Component/Layouts/Layout'
+import '@fortawesome/fontawesome-free/css/all.min.css'
+import 'bootstrap-css-only/css/bootstrap.min.css'
+import 'mdbreact/dist/css/mdb.css'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
